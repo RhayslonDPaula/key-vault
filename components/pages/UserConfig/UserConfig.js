bea919cb97b43ce29458b8ebe9c3fdd39b28763c
RHAYSLON MATHEUS DE PAULA CUNHA <37088883+RhayslonDPaula@users.noreply.github.com>
2022-12-05 19:43:52 -0300

    Revert "add enquadramento"
    This reverts commit 54787013a242c729667f907a41276840f7ebac02.

--- components/pages/UserConfig/UserConfig.js
+++ components/pages/UserConfig/UserConfig.js
@@ -12,8 +12,9 @@ function BodyRegister() {
   const [open, setOpen] = useState(false);
   return (
     
-    <Container className ="p-5">
-    <Card className ="p-4" bg= "shadow text-bg-kv-primary-700" style={{ width: '25' }}>
+    <Container>
+    
+    <Card  bg= "shadow text-bg-kv-primary-700" style={{ width: '25' }}>
       <Card.Body>
      <Stack gap={2} className="col-md-5 mx-auto">
      <Button variant= "shadow text-bg-kv-primary-900">
