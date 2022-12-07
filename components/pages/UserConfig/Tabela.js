efbe7f1bcbfa0908eb3d55f2239451b5105be0e6
RHAYSLON MATHEUS DE PAULA CUNHA <37088883+RhayslonDPaula@users.noreply.github.com>
2022-12-07 02:54:51 -0300

    created the table component
    component for the transactions component

---
+++ components/pages/UserConfig/Tabela.js
@@ -0,0 +1,14 @@
+import Button from 'react-bootstrap/Button';
+import React, { useState } from 'react';
+import Form from 'react-bootstrap/Form';
+import Container from 'react-bootstrap/Container';
+import Stack from 'react-bootstrap/Stack';
+import Card from 'react-bootstrap/Card';
+import Collapse from 'react-bootstrap/Collapse';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import Col from 'react-bootstrap/Col';
+import Nav from 'react-bootstrap/Nav';
+import Row from 'react-bootstrap/Row';
+import Tab from 'react-bootstrap/Tab';
+import Table from 'react-bootstrap/Table';
+
